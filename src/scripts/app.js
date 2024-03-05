7a69d2c5c35e78c6d4dd62cac614ab34cd6c8bad
Mazmanyan Yura <mazmanya@mail.ru>
2024-03-06 01:35:19 +0300

    Функция LOADDATA- загружает данные с localStorage. Функция SAVEDATA- сохраняет полученные данные с localStorage

---
+++ src/scripts/app.js
@@ -0,0 +1,18 @@
+"use stict";
+let habbits = [];
+const HABBIT_KEY = "HABBIT_KEY";
+
+/* Utils */
+
+function loadData() {
+  const habbitsString = localStorage.getItem(HABBIT_KEY);
+  const habbitArray = JSON.parse(habbitsString);
+  if (Array.isArray(habbitArray)) habbits = habbitArray;
+}
+function saveData() {
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+(() => {
+  loadData();
+})();
