73e635629e49ab2b75d86116b4794216552089f4
Mazmanyan Yura <mazmanya@mail.ru>
2024-03-07 23:21:52 +0300

    Создание функции RederHead. Делает  рендер меню в блоке Content, изменяя состояние прогресса и заголвка

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,6 +5,11 @@ const HABBIT_KEY = "HABBIT_KEY";
 /* Page */
 const page = {
   menu: document.querySelector(".menu__list"),
+  header: {
+    h1: document.querySelector("h1"),
+    progressPercent: document.querySelector(".progress__percent"),
+    progressCoverBar: document.querySelector(".progress__cover-bar"),
+  },
 };
 
 /* Utils */
@@ -47,10 +52,23 @@ function rerenderMenu(activeHabbit) {
       : existed.classList.remove("menu__item_active");
   }
 }
+function renderHead(activeHabbit) {
+  if (!activeHabbit) {
+    return;
+  }
+  page.header.h1.innerText = activeHabbit.name;
+  const progress =
+    activeHabbit.days.length / activeHabbit.target > 1
+      ? 100
+      : (activeHabbit.days.length / activeHabbit.target) * 100;
+  page.header.progressPercent.innerText = `${progress.toFixed(0)}%`;
+  page.header.progressCoverBar.setAttribute("style", `width: ${progress}%`);
+}
 
 function rerender(activeHabbitId) {
   const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
   rerenderMenu(activeHabbit);
+  renderHead(activeHabbit);
 }
 
 /* INIT */
