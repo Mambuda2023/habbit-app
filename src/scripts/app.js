91a7c1efc0a91116cc8dc6e21cfd98bb88a39a06
Mazmanyan Yura <mazmanya@mail.ru>
2024-03-18 14:04:51 +0300

    Создание функции VALIDATEFORM. Эта функции валидирует поля в POPUPFORM

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,7 @@
 "use strict";
-let habbits = [];
-const HABBIT_KEY = "HABBIT_KEY";
-let globalActiveHabbitId;
+let habits = [];
+const HABIT_KEY = "HABIT_KEY";
+let globalActiveHabitId;
 
 /* Page */
 const page = {
@@ -26,65 +26,89 @@ const page = {
 /* Utils */
 
 function loadData() {
-  const habbitsString = localStorage.getItem(HABBIT_KEY);
-  const habbitArray = JSON.parse(habbitsString);
-  if (Array.isArray(habbitArray)) {
-    habbits = habbitArray;
+  const habitsString = localStorage.getItem(HABIT_KEY);
+  const habitArray = JSON.parse(habitsString);
+  if (Array.isArray(habitArray)) {
+    habits = habitArray;
   }
 }
 
 function saveData() {
-  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+  localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
+}
+
+
+function validateForm(form, fields) {
+  const formData = new FormData(form);
+  res = {};
+  for (const field of fields) {
+    const fieldValue = data.get(field);
+    form[field].classList.remove("error");
+    if (!fieldValue) {
+      form[field].classList.add("error");
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if (!res[field]) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
+    return;
+  }
+  return res;
 }
 
 /* RENDER */
-function rerenderMenu(activeHabbit) {
-  for (const habbit of habbits) {
-    const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+function rerenderMenu(activeHabit) {
+  for (const habit of habits) {
+    const existed = document.querySelector(`[menu-habbit-id="${habit.id}"]`);
     if (!existed) {
       const element = document.createElement("button");
       element.classList.add("icon");
-      element.setAttribute("menu-habbit-id", habbit.id);
-      element.innerHTML = `<img src="images/${habbit.icon}.svg" alt="${habbit.name}"/>`;
+      element.setAttribute("menu-habbit-id", habit.id);
+      element.innerHTML = `<img src="images/${habit.icon}.svg" alt="${habit.name}"/>`;
       element.addEventListener("click", () => {
-        rerender(habbit.id);
+        rerender(habit.id);
       });
-      if (activeHabbit.id === habbit.id)
+      if (activeHabit.id === habit.id)
         element.classList.add("menu__item_active");
 
       page.menu.appendChild(element);
       continue;
     }
 
-    activeHabbit.id === habbit.id
+    activeHabit.id === habit.id
       ? existed.classList.add("icon_active")
       : existed.classList.remove("icon_active");
   }
 }
-function renderHead(activeHabbit) {
-  page.header.h1.innerText = activeHabbit.name;
+function renderHead(activeHabit) {
+  page.header.h1.innerText = activeHabit.name;
   const progress =
-    activeHabbit.days.length / activeHabbit.target > 1
+    activeHabit.days.length / activeHabit.target > 1
       ? 100
-      : (activeHabbit.days.length / activeHabbit.target) * 100;
+      : (activeHabit.days.length / activeHabit.target) * 100;
   page.header.progressPercent.innerText = `${progress.toFixed(0)}%`;
   page.header.progressCoverBar.setAttribute("style", `width: ${progress}%`);
 }
 
-function renderContent(activeHabbit) {
+function renderContent(activeHabit) {
   page.content.daysContainer.innerHTML = "";
-  for (const index in activeHabbit.days) {
+  for (const index in activeHabit.days) {
     const element = document.createElement("div");
     element.classList.add("habbit");
     element.innerHTML = ` 
     <h2 class="habbit__day">День ${Number(index) + 1}</h2>
-    <p class="habbit__comment">${activeHabbit.days[index].comment}</p>
+    <p class="habbit__comment">${activeHabit.days[index].comment}</p>
     <button class="habbit__delete" onclick=deleteDay(${index})>
       <img src="./images/delete-button.svg" alt="Удалить день ${index + 1}" />
     </button>`;
     page.content.daysContainer.appendChild(element);
   }
-  page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`;
+  page.content.nextDay.innerHTML = `День ${activeHabit.days.length + 1}`;
 }
 //Working with days
 function addDays(event) {
@@ -96,40 +120,40 @@ function addDays(event) {
   if (!comment) {
     form["comment"].classList.add("error");
   }
-  habbits = habbits.map((habbit) => {
-    if (habbit.id === globalActiveHabbitId) {
-      return { ...habbit, days: habbit.days.concat([{ comment }]) };
+  habits = habits.map((habit) => {
+    if (habit.id === globalActiveHabitId) {
+      return { ...habit, days: habit.days.concat([{ comment }]) };
     }
-    return habbit;
+    return habit;
   });
   form["comment"].value = "";
-  rerender(globalActiveHabbitId);
+  rerender(globalActiveHabitId);
   saveData();
 }
 
 function deleteDay(index) {
-  habbits = habbits.map((habbit) => {
-    if (habbit.id === globalActiveHabbitId) {
-      habbit.days.splice(index, 1);
+  habits = habits.map((habit) => {
+    if (habit.id === globalActiveHabitId) {
+      habit.days.splice(index, 1);
       return {
-        ...habbit,
-        days: habbit.days,
+        ...habit,
+        days: habit.days,
       };
     }
-    return habbit;
+    return habit;
   });
-  rerender(globalActiveHabbitId);
+  rerender(globalActiveHabitId);
   saveData();
 }
 //Working with days
 
-function rerender(activeHabbitId) {
-  globalActiveHabbitId = activeHabbitId;
-  const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
-  if (!activeHabbit) return;
-  rerenderMenu(activeHabbit);
-  renderHead(activeHabbit);
-  renderContent(activeHabbit);
+function rerender(activeHabitId) {
+  globalActiveHabitId = activeHabitId;
+  const activeHabit = habits.find((habit) => habit.id === activeHabitId);
+  if (!activeHabit) return;
+  rerenderMenu(activeHabit);
+  renderHead(activeHabit);
+  renderContent(activeHabit);
 }
 //Скрытие popup
 page.popup.closeButton.addEventListener("click", () => {
@@ -145,16 +169,19 @@ page.popup.openButton.addEventListener("click", () => {
   page.popup.cover.classList.remove("cover__hidden");
 });
 //Скрытие popup
-//Working with habbits
+//Working with habits
 function setIcon(context, icon) {
   page.popup.iconField.value = icon;
   const activeIcon = document.querySelector(".icon.icon_active");
   activeIcon.classList.remove("icon_active");
   context.classList.add("icon_active");
 }
-//Working with habbits
+//Working with habits
+function addHabit(event) {
+  event.preventDefault();
+}
 /* INIT */
 (() => {
   loadData();
-  rerender(habbits[0].id);
+  rerender(habits[0].id);
 })();
